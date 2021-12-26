6fe984085d4f54c58f9f740475105277ce0a9928
Philip <1pluswhat=gh>
2021-12-27 00:04:44 +0100

    Initial Commit

---
+++ VRCNeotaInterface/VRCNeotaInterface.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace VRCNeotaInterface
+{
+    public class VrcNeotaInterface : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            LoggerInstance.Msg("Nota come back pls");
+        }
+    }
+}
